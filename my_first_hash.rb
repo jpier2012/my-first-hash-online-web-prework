8f942fb93b33c9d634e207f1a200edbf2e877014
James Pier <jpier2012@gmail.com>
2019-04-04 12:03:51 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  hash = { "name" => "James", "height" => "5'9\"", "weight" => "~180 lbs", "hair color" => "blonde", "eye color" => "blue" }
-
+  jim_hash = { "name" => "James", "height" => "5'9\"", "weight" => "~180 lbs", "hair color" => "blonde", "eye color" => "blue" }
 end
 
 
@@ -9,7 +8,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-
+  the_manifest = { "whale bone corest" => 5, "porcelain vase" => 2, "oil painting" => 3}
 
 end
 
