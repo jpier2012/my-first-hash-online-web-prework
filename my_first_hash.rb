67a685c29240dbc3377f5f79f819b8c80890c63b
James Pier <jpier2012@gmail.com>
2019-04-04 12:07:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,12 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  jim_hash = { "name" => "James", "height" => "5'9\"", "weight" => "~180 lbs", "hair color" => "blonde", "eye color" => "blue" }
+  jim_hash = { 
+              "name" => "James", 
+              "height" => "5'9\"", 
+              "weight" => "~180 lbs", 
+              "hair color" => "blonde", 
+              "eye color" => "blue" 
+             }
 end
 
 
@@ -8,7 +14,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-  the_manifest = { "whale bone corest" => 5, "porcelain vase" => 2, "oil painting" => 3}
+  the_manifest = { "whale bone corests" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
 end
 
@@ -18,9 +24,8 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
 
 def adding
